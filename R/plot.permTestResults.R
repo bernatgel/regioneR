d7bb736552e7d36df1c944e5443fc73dd7988989
Bernat Gel <bgel@imppc.org>
2015-06-04 13:37:53 +0000

    Fixed two bugs in the plotting function preventing the use of the "main" parameter Fixed a problem in plot.permTestResults where the shaded area was incorrectly placed in strongly zero-inflated distributions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/regioneR@104557 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plot.permTestResults.R
+++ R/plot.permTestResults.R
@@ -33,8 +33,8 @@
 #' @export 
 
 
-plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
-
+plot.permTestResults<-function(x, pvalthres=0.05, plotType="Tailed", main="", xlab=NULL, ylab="", ...){
+  
   old.scipen <- options()$scipen
   
   options(scipen=999)
@@ -44,12 +44,8 @@ plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
   plotType<-match.arg(plotType,c("Area","Tailed"))
   
   
-  if(!hasArg(xlab)) xlab <- paste0(x$evaluate.function.name)
-  if(!hasArg(ylab)) ylab <- ""
-  if(!hasArg(main)) main <- ""
-  
-    
-  if(nchar(main)>0) main <- paste0(main,"\n")
+  if(is.null(xlab)) xlab <- paste0(x$evaluate.function.name)
+  if(nchar(main)>0) main <- paste0(main, "\n")  
   
   alternative<-x$alternative
   xcoords<-x$permuted
@@ -59,7 +55,7 @@ plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
   mperm<-mean(xcoords,na.rm=TRUE)
   mobs<-x$observed
   zscore<-round(x$zscore,3)
-
+  
   if(is.finite(zscore)){
     y<-dnorm(xcoords,mean=mean(xcoords,na.rm=TRUE),sd=sd(xcoords,na.rm=TRUE))
     xhist<-hist(xcoords,breaks=30,plot=FALSE)$density
@@ -68,16 +64,22 @@ plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
     if (alternative=="greater") aux<-qnorm((1-pvalthres),mean=mean(xcoords,na.rm=TRUE),sd=sd(xcoords,na.rm=TRUE))
     if (alternative=="less") aux<-qnorm(pvalthres,mean=mean(xcoords,na.rm=TRUE),sd=sd(xcoords,na.rm=TRUE))
     
-    hist(xcoords, prob = TRUE, ylim = c(0,ymax), breaks = 30, xlim = c(min(mobs,min(xcoords,na.rm=TRUE),min(aux,na.rm=TRUE),na.rm=TRUE), max(mobs,max(xcoords,na.rm=TRUE),min(aux,na.rm=TRUE),na.rm=TRUE)), las = 1, col = "lightgray", border = "lightgray", xlab=xlab, ylab=ylab, main=paste(main, "p-value: ", pval, "\n Z-score: ", zscore, "\n n perm: ", nperm, "\n randomization: ", paste0(x$randomize.function.name)), cex.main=0.8, ...)
+    xmin<-min(mobs,min(xcoords,na.rm=TRUE),min(aux,na.rm=TRUE),na.rm=TRUE)
+    xmax<-max(mobs,max(xcoords,na.rm=TRUE),max(aux,na.rm=TRUE),na.rm=TRUE)
+    
+    hist(xcoords, prob = TRUE, ylim = c(0,ymax), breaks = 30, xlim = c(xmin,xmax),
+         las = 1, col = "lightgray", border = "lightgray", xlab=xlab, ylab=ylab, main=paste(main, "p-value: ",
+                                                                                            pval, "\n Z-score: ", zscore, "\n n perm: ", nperm, "\n randomization: ", paste0(x$randomize.function.name)),
+         cex.main=0.8, ...)
     
     if(plotType=="Area"){
       if(alternative=="greater"){
-        polygon(c(aux,aux,max(mobs,max(xcoords,na.rm=TRUE),na.rm=TRUE),max(mobs,max(xcoords,na.rm=TRUE),na.rm=TRUE)),c(max(y,na.rm=TRUE),0,0,max(y,na.rm=TRUE)),col="red",density=10,border="white")
+        polygon(c(aux,aux,xmax,xmax),c(max(y,na.rm=TRUE),0,0,max(y,na.rm=TRUE)),col="red",density=10,border="white")
         lines(c(aux,aux),c(0,ymax*0.8),col="red",lwd=3)
         text(aux,ymax*0.9,bquote(alpha==.(pvalthres)),cex=0.8,pos=4)
       }
       if(alternative=="less"){
-        polygon(c(aux,aux,min(mobs,min(xcoords,na.rm=TRUE),na.rm=TRUE),min(mobs,min(xcoords,na.rm=TRUE),na.rm=TRUE)),c(max(y,na.rm=TRUE),0,0,max(y,na.rm=TRUE)),col="red",density=10,border="white")
+        polygon(c(aux,aux,xmin,xmin),c(max(y,na.rm=TRUE),0,0,max(y,na.rm=TRUE)),col="red",density=10,border="white")
         lines(c(aux,aux),c(0,ymax*0.8),col="red",lwd=3)
         text(aux,ymax*0.9,bquote(alpha==.(pvalthres)),cex=0.8)
       }
@@ -85,14 +87,14 @@ plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
     
     if(plotType=="Tailed"){
       if(alternative=="greater"){
-        aux3<-seq(aux,max(max(xcoords,na.rm=TRUE),mobs,na.rm=TRUE),length=50)
+        aux3<-seq(aux,xmax,length=50)
         y3<-dnorm(aux3,mean(xcoords,na.rm=TRUE),sd(xcoords,na.rm=TRUE))
         polygon(c(aux3[1],aux3,aux3[length(aux3)]),c(0,y3,0),col="red",density=10,border="white")
         lines(c(aux,aux),c(0,ymax*0.8),col="red",lwd=3)
         text(aux,ymax*0.9,bquote(alpha==.(pvalthres)),cex=0.8)
       }
       if(alternative=="less"){
-        aux3<-seq(aux,min(min(xcoords,na.rm=TRUE),mobs,na.rm=TRUE),length=50)
+        aux3<-seq(aux,xmin,length=50)
         y3<-dnorm(aux3,mean(xcoords,na.rm=TRUE),sd(xcoords,na.rm=TRUE))
         polygon(c(aux3[1],aux3,aux3[length(aux3)]),c(0,y3,0),col="red",density=10,border="white")
         lines(c(aux,aux),c(0,ymax*0.8),col="red",lwd=3)
@@ -124,13 +126,12 @@ plot.permTestResults<-function(x, pvalthres=0.05, plotType="Area", ...){
     text(mobs,ymax*0.9,expression(Ev[obs]),cex=0.8)
     arrows(mperm,ymax*0.75,mobs,ymax*0.75,length=0.1,code=3)
     box(lwd=1.2)
-    ?expression
     
     
     warning(paste0("all permuted values are equal to ",xcoords[1],". It is not posible to adjust a normal distribution nor to compute a Z-score."))
     
   }
-
+  
   options(scipen=old.scipen)
   
 }
